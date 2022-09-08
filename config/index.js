a70dbeb39caae137f861ac6e655dedae5efc122a
冯浩宇 <102705820+webfiregogo@users.noreply.github.com>
2022-09-08 16:49:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -74,7 +74,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "QrBMsOrzVDf-ldKbk63WdEVrHCyBfVr6iOaDnnLiO1E",
+  CALLBACK_TEMPLATE_ID: "",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -82,7 +82,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ojhHX5rYnlhP1C_roqh_qfebYgdY",
+      id: "",
     }, 
   ],
     
